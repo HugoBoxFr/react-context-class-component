223d51b9470ff5bc595c0f132c2bb287b07dc9d3
HugoBoxFr <hugo.contactweb@gmail.com>
2021-01-17 23:59:59 +0100

    lesson7 useContext hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,7 @@
-import React, { Component, useContext } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 
-// class version
+// class context version
 // class BookList extends Component {
 //     static contextType = ThemeContext;
 //     render() {
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 
-class ThemeToggle extends Component {
-    static contextType = ThemeContext;
+
+// class context version
+// class ThemeToggle extends Component {
+//     static contextType = ThemeContext;
     
-    render() {
-        const { toggleTheme } = this.context;
+//     render() {
+//         const { toggleTheme } = this.context;
 
-        return (
-            <div>
-                <button onClick={toggleTheme}>Theme</button>
-            </div>
-        )
-    }
+//         return (
+//             <div>
+//                 <button onClick={toggleTheme}>Theme</button>
+//             </div>
+//         )
+//     }
+// }
+
+const ThemToggle = () => {
+    const { toggleTheme } = useContext(ThemeContext);
+
+    return ( 
+        <div>
+            <button onClick={toggleTheme}>Theme</button>
+        </div>
+    );
 }
-
-export default ThemeToggle;
+ 
+export default ThemToggle;
