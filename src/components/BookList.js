c15d0a80e9c2dffbbccdb1312f6b45d45c8c42a8
HugoBoxFr <hugo.contactweb@gmail.com>
2021-01-17 18:12:21 +0100

    lesson 1: class component with contextType

---
+++ src/components/BookList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { ThemeContext } from '../context/ThemeContext';
+
+class BookList extends Component {
+    static contextType = ThemeContext;
+
+    render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+
+        return (
+            <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+                <ul>
+                    <li style={{ background: theme.ui }}>kings</li>
+                    <li style={{ background: theme.ui }}>queens</li>
+                    <li style={{ background: theme.ui }}>final</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default BookList;
